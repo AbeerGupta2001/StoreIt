6841f54f385ad19c2170a5a33cef1220837f0158
AbeerGupta2001 <abeergupta2001@gmail.com>
2025-01-26 03:58:16 +0530

    done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,15 +3,16 @@ import MobileNavigation from "@/components/MobileNavigation"
 import Sidebar from "@/components/Sidebar"
 import { getCurrentUser } from "@/lib/actions/user.actions"
 import { redirect } from "next/navigation"
-
-
 import { ReactNode } from "react"
 
+
+export const dynamic = "force-dynamic"
+
 const Layout = async ({children}:{children:ReactNode}) => {
-  const currentUser = await getCurrentUser().catch((error)=>{
-    console.log(error)
-    redirect("/sign-in");
-  });
+  const currentUser = await getCurrentUser();
+  if(!currentUser){
+    redirect("/sign-in")
+  }
   
   return (
     <main className="flex h-screen">
